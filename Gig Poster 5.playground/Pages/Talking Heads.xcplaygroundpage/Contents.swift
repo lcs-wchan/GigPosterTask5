35bbfc9d666579983a8413dff2ca24106d4aa061
WilliamChan <wchan@lcs.on.ca>
2020-02-12 14:49:32 -0500

    finished gig poster 5

--- Gig Poster 5.playground/Pages/Talking Heads.xcplaygroundpage/Contents.swift
+++ Gig Poster 5.playground/Pages/Talking Heads.xcplaygroundpage/Contents.swift
@@ -60,3 +60,50 @@ let brightYellow = Color(hue: 46, saturation: 71, brightness: 98, alpha: 100)
  */
 PlaygroundPage.current.liveView = canvas
 
+// draw orange background
+canvas.fillColor = deepOrange
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+// draw white trangle
+canvas.drawShapesWithBorders = false
+canvas.fillColor = offWhite
+var whiteTriangle: [Point] = []
+whiteTriangle.append(Point(x: 0, y: 200))
+whiteTriangle.append(Point(x: 0, y: 600))
+whiteTriangle.append(Point(x: 400, y: 600))
+canvas.drawCustomShape(with: whiteTriangle)
+
+// draw yellow trangle
+canvas.fillColor = brightYellow
+var yellowTriangle: [Point] = []
+yellowTriangle.append(Point(x: 0, y: 200))
+yellowTriangle.append(Point(x: 400, y: 600))
+yellowTriangle.append(Point(x: 400, y: 200))
+canvas.drawCustomShape(with: yellowTriangle)
+
+// draw small orange triangles
+for y in stride (from: 200, through: 560, by: 40){
+    for x in stride (from: 0, through: 400, by: 40){
+        canvas.fillColor = deepOrange
+
+        var trangle: [Point] = []
+        trangle.append(Point(x: x, y: y))
+        trangle.append(Point(x: x, y: y+40))
+        trangle.append(Point(x: x+40, y: y+40))
+        canvas.drawCustomShape(with: trangle)
+    }
+}
+
+// draw text
+canvas.textColor = offWhite
+
+canvas.drawText(message: "friday, saturday, sunday", at: Point(x: 17, y: 40), size: 8)
+canvas.drawText(message: "september 12, 13, 14, 1975", at: Point(x: 17, y: 20), size: 8)
+
+canvas.drawText(message: "at cbgb and omfug", at: Point(x: 150, y: 40), size: 8)
+canvas.drawText(message: "315 bowery, new york city", at: Point(x: 150, y: 20), size: 8)
+
+canvas.drawText(message: "also appearing:", at: Point(x: 275, y: 40), size: 8)
+canvas.drawText(message: "from brooklyn, the shirts", at: Point(x: 275, y: 20), size: 8)
+
+canvas.drawText(message: "talking heads", at: Point(x: 20, y: 150), size: 40)
